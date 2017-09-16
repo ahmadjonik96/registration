ca975d4f06069bddd95704ac6099a277cd639dde
ahmadjonik96@mail.ru <your_email@youremail.comA>
2017-09-17 01:21:58 +0900

    Initial commit

---
+++ login/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from login.serializers import UserSerializer
+from django.contrib.auth.models import User
+
+
+class UserCreate(APIView):
+    """
+    Creates the user.
+    """
+
+    def post(self, request, format='json'):
+        return Response('hello')
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                "creation of token after sucessiful registration"
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+            "creation of token after sucessiful registration"
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Create your views here.
